355ef874c171b65c70e6aab97f56cca5f547b519
Alejandro Canon <al3jandrocan0n@gmail.com>
2024-02-06 12:18:01 -0500

    Feature: adding database and restapi

--- components/atoms/CapitalInput.tsx
+++ components/atoms/CapitalInput.tsx
@@ -2,7 +2,7 @@ const CapitalInput = () => (
   <input
     type="text"
     placeholder="Enter the capital"
-    className="text-center border-2 p-2 rounded-md outline-yellow-500 cursor-pointer"
+    className="text-center border-2 p-2 rounded-md ring-yellow-500 ring-2 outline-yellow-500 cursor-pointer"
   />
 );
 
